d128efd11c85a99d1fc90d2430913210a71f322a
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-11-17 13:09:09 +0100

    AddServiceExtensionsClass

--- src/TaskManagementSystem/TaskManagementSystem.Api/Program.cs
+++ src/TaskManagementSystem/TaskManagementSystem.Api/Program.cs
@@ -1,10 +1,16 @@
+using TaskManagementSystem.Api.ServiceExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCorsImplementation(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
---
+++ src/TaskManagementSystem/TaskManagementSystem.Api/ServiceExtensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManagementSystem.Api.ServiceExtensions;
+
+internal static class ApplicationServiceExtensions
+{
+    internal static void AddCorsImplementation(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(opts =>
+        {
+            opts.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+            });
+        });
+    }
+}
