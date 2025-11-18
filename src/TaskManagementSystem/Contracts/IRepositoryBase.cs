b68af0a99d912832bca786cef11be0b97f3fdacc
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-11-18 04:14:50 +0100

    CreateGenericRepository

---
+++ src/TaskManagementSystem/Contracts/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts;
+
+public interface IRepositoryBase<T>
+{
+    Task Create(T entity);
+    void UpdateEntity(T entity);
+    void DeleteEntity(T entity);
+    IQueryable<T> FindAll(bool trackChanges = true, bool hasQueryFilter = true);
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition, bool trackChanges = true, bool hasQueryFilter = true);
+    Task<int> ExecuteCustomQuery(string command, params object[] parameters);
+    Task<IQueryable<T>> CustomeDatabaseQuery(string command, params object[] parameters);
+}
---
+++ src/TaskManagementSystem/Repository/RepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+internal sealed class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    private readonly RepositoryContext _repositoryContext;
+
+    public RepositoryBase(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+    }
+
+    public async Task Create(T entity)
+    {
+        await _repositoryContext.Set<T>().AddAsync(entity);
+    }
+
+    public async Task<IQueryable<T>> CustomeDatabaseQuery(string command, params object[] parameters)
+    {
+        return await Task.FromResult(_repositoryContext.Database.SqlQueryRaw<T>(command, parameters));
+    }
+
+    public void DeleteEntity(T entity)
+    {
+        _repositoryContext.Set<T>().Remove(entity);
+    }
+
+    public async Task<int> ExecuteCustomQuery(string command, params object[] parameters)
+    {
+        return await _repositoryContext.Database.ExecuteSqlRawAsync(command, parameters);
+    }
+
+    public IQueryable<T> FindAll(bool trackChanges = true, bool hasQueryFilter = true)
+    {
+        IQueryable<T> entities = _repositoryContext.Set<T>();
+
+        if (!trackChanges)
+            entities.AsNoTracking();
+
+        if(!hasQueryFilter)
+            entities.IgnoreQueryFilters();
+
+        return entities;
+    }
+
+    public IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> condition, bool trackChanges = true, bool hasQueryFilter = true)
+    {
+        var entities = _repositoryContext.Set<T>()
+                                .Where(condition);
+
+        if(!trackChanges)
+            entities.AsNoTracking();
+        if(!hasQueryFilter)
+            entities.IgnoreAutoIncludes();  
+
+        return entities;
+    }
+
+    public void UpdateEntity(T entity)
+    {
+       _repositoryContext.Set<T>().Update(entity);
+    }
+}
