fe2da702ca709237d941ab08ca0678885d6c6eaa
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-12-22 10:07:11 +0100

    ConfigureEditCreatedTask

--- src/TaskManagementSystem/TaskManagementSystem.Client/Program.cs
+++ src/TaskManagementSystem/TaskManagementSystem.Client/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddScoped<GetUnitsHandler>();
 builder.Services.AddScoped<GetUsersByUnitHandler>();
 builder.Services.AddScoped<AddNewUserTaskHandler>();
 builder.Services.AddScoped<AddNewTaskHandler>();
+builder.Services.AddScoped<EditCreatedTaskHandler>();
 
 //Clients
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
